71b78fca6b867b048241dc4a494ad40c956f6ade
ArrRon9978 <70335679+ArrRon9978@users.noreply.github.com>
2020-08-31 15:54:29 -0700

    Add files via upload

---
+++ Arriaga introLab.cpp
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: ronniearriaga
+ *
+ * Created on June 1, 2020, 1:26 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout<<"Hello World!\n";
+    cout<<"Welcome to CIS 17-A";
+    
+    
+    return 0;
+}
+
